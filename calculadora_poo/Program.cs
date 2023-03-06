ec23eed9506ea6537a51c2deda102f62ba1d33f3
Hiromi Acosta Quevedo <mafequevedo1@gmail.com>
2023-03-06 16:42:43 -0500

    añadir comentarios
    Se colocan los textos previos a la solución de las operaciones

--- calculadora_poo/Program.cs
+++ calculadora_poo/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace calculadora_poo
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            //Solicitar valores y almacenarlos 
+
+            Console.WriteLine("Ingrese el valor 1");
+            string value1 = Console.ReadLine();
+            int value1Number = Int32.Parse(value1);
+
+            Console.WriteLine("Ingrese el valor 2");
+            string value2 = Console.ReadLine();
+            int value2Number = Int32.Parse(value2);
+
+            // Sumar los números
+            Console.WriteLine("La suma de los números ingresado es:");
+            Console.WriteLine(value1Number + value2Number);
+            // Restar los números
+            Console.WriteLine("La resta de los números ingresado es:");
+            Console.WriteLine(value1Number - value2Number);
+            // Multiplicar los números
+            Console.WriteLine("La multiplicación de los números ingresado es:");
+            Console.WriteLine(value1Number * value2Number);
+            // Dividir los números
+            Console.WriteLine("La división de los números ingresado es:");
+            Console.WriteLine(value1Number / value2Number);
+
         }
     }
 }
